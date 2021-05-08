f74cbe3ec36ff6d9a0bb2e0ca4169e0da2a26910
Dzianis Lukayenka <dav08649@gmail.com>
2021-05-08 23:35:46 +0300

    use hoc for soundfont provider

--- src/adapters/Soundfont/SoundfontProvider.tsx
+++ src/adapters/Soundfont/SoundfontProvider.tsx
@@ -1,107 +1,113 @@
 import { MidiValue } from "domain/note";
-import { Component, ReactElement } from "react";
+import { Component, ComponentType } from "react";
 import { InstrumentName, Player } from "soundfont-player";
 import { Optional } from "domain/types";
 import { AudioNodesRegistry, DEFAULT_INSTRUMENT } from "domain/sound";
 import * as Soundfont from 'soundfont-player';
 
-interface ProviderProps {
+export interface ProviderProps {
     instrument: InstrumentName;
     AudioContext: AudioContextType;
-    render(props: ProvidedProps): ReactElement;
 }
 
-interface ProvidedProps {
+interface InjectedProps {
     loading: boolean;
     play(note: MidiValue): Promise<void>;
     stop(note: MidiValue): Promise<void>;
 }
 
-interface ProviderState {
+export interface ProviderState {
     loading: boolean;
     current: Optional<InstrumentName>;
 }
 
-export class SoundfontProvider extends Component<ProviderProps, ProviderState> {
-    public static defaultProps = {
-        instrument: DEFAULT_INSTRUMENT,
-    }
+export function withInstrument<TProps extends InjectedProps = InjectedProps>(WrappedComponent: ComponentType<TProps>) {
+    const displayName = WrappedComponent.displayName || WrappedComponent.name || "Component";
 
-    public state: ProviderState = {
-        loading: false,
-        current: null,
-    };
+    return class WithInstrument extends Component<ProviderProps, ProviderState> {
+        public static displayName = `withInstrument(${displayName})`;
 
-    private audio: AudioContext;
-    private player: Optional<Player> = null;
-    private activeNodes: AudioNodesRegistry = {};
-
-    constructor(props: ProviderProps) {
-        super(props);
-
-        const { AudioContext } = this.props;
-        this.audio = new AudioContext();
-    }
-
-    public componentDidMount() {
-        const { instrument } = this.props;
-        this.load(instrument);
-    }
-
-    public shouldComponentUpdate({ instrument }: ProviderProps) {
-        return this.state.current !== instrument;
-    }
-
-    public componentDidUpdate({ instrument: prevInstrument }: ProviderProps) {
-        const { instrument } = this.props;
-
-        if (instrument && instrument !== prevInstrument) {
+        public static defaultProps = {
+            instrument: DEFAULT_INSTRUMENT,
+        }
+    
+        public state: ProviderState = {
+            loading: false,
+            current: null,
+        };
+    
+        private audio: AudioContext;
+        private player: Optional<Player> = null;
+        private activeNodes: AudioNodesRegistry = {};
+    
+        constructor(props: ProviderProps) {
+            super(props);
+    
+            const { AudioContext } = this.props;
+            this.audio = new AudioContext();
+        }
+    
+        public componentDidMount() {
+            const { instrument } = this.props;
             this.load(instrument);
         }
-    }
-
-    public play = async (note: MidiValue) => {
-        await this.resume();
-
-        if (!this.player) {
-            return;
+    
+        public shouldComponentUpdate({ instrument }: ProviderProps) {
+            return this.state.current !== instrument;
+        }
+    
+        public componentDidUpdate({ instrument: prevInstrument }: ProviderProps) {
+            const { instrument } = this.props;
+    
+            if (instrument && instrument !== prevInstrument) {
+                this.load(instrument);
+            }
+        }
+    
+        public play = async (note: MidiValue) => {
+            await this.resume();
+    
+            if (!this.player) {
+                return;
+            }
+    
+            const node = this.player.play(note.toString());
+            this.activeNodes = { ...this.activeNodes, [note]: node };
+        }
+    
+        public stop = async (note: MidiValue) => {
+            await this.resume();
+    
+            if (!this.activeNodes[note]) {
+                return;
+            }
+    
+            this.activeNodes[note]!.stop();
+            this.activeNodes = { ...this.activeNodes, [note]: null };
+        }
+    
+        private load = async (instrument: InstrumentName = DEFAULT_INSTRUMENT) => {
+            this.setState({ loading: true });
+            
+            this.player = await Soundfont.instrument(this.audio, instrument);
+            
+            this.setState({ loading: false, current: instrument });
+        }
+    
+        private resume = async () => {
+            return this.audio.state === 'suspended' ? await this.audio.resume() : Promise.resolve();
         }
 
-        const node = this.player.play(note.toString());
-        this.activeNodes = { ...this.activeNodes, [note]: node };
-    }
+        public render() {
+            const injected = {
+                loading: this.state.loading,
+                play: this.play,
+                stop: this.stop,
+            } as InjectedProps;
 
-    public stop = async (note: MidiValue) => {
-        await this.resume();
-
-        if (!this.activeNodes[note]) {
-            return;
+            return (
+                <WrappedComponent {...this.props} {...(injected as TProps)} />
+            );
         }
-
-        this.activeNodes[note]!.stop();
-        this.activeNodes = { ...this.activeNodes, [note]: null };
-    }
-
-    public render() {
-        const { render } = this.props;
-        const { loading } = this.state;
-
-        return render({
-            loading,
-            play: this.play,
-            stop: this.stop,
-        });
-    }
-
-    private load = async (instrument: InstrumentName = DEFAULT_INSTRUMENT) => {
-        this.setState({ loading: true });
-        
-        this.player = await Soundfont.instrument(this.audio, instrument);
-        
-        this.setState({ loading: false, current: instrument });
-    }
-
-    private resume = async () => {
-        return this.audio.state === 'suspended' ? await this.audio.resume() : Promise.resolve();
     }
 }
--- src/components/Keyboard/WithInstrument.tsx
+++ src/components/Keyboard/WithInstrument.tsx
@@ -2,17 +2,18 @@ import React, { FunctionComponent } from 'react';
 import { useAudioContext } from 'components/AudioContextProvider/useAudioContext';
 import { Keyboard } from './Keyboard';
 import { useInstrument } from 'state/Instrument/Context';
-import { SoundfontProvider } from 'adapters/Soundfont/SoundfontProvider';
+import { withInstrument } from 'adapters/Soundfont/SoundfontProvider';
+
+const WrappedKeyboard = withInstrument(Keyboard);
 
 export const KeyboardWithInstrument: FunctionComponent = () => {
     const context = useAudioContext()!;
     const { instrument } = useInstrument();
 
     return (
-        <SoundfontProvider
+        <WrappedKeyboard
             AudioContext={context}
             instrument={instrument}
-            render={(props) => <Keyboard {...props} />}
         />
     )
 }
